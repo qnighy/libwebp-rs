2668407749b45f0291c9c6f52bc593688c0c0c2f
Masaki Hara <ackie.h.gmai@gmail.com>
2020-03-07 14:11:55 +0900

    Implement WebPDecode*Into

--- src/decode.rs
+++ src/decode.rs
@@ -135,6 +135,164 @@ pub fn WebPDecodeYUV(data: &[u8]) -> Result<(u32, u32, u32, u32, WebpYuvBox), We
     }
 }
 
+#[allow(non_snake_case)]
+pub fn WebPDecodeRGBAInto(
+    data: &[u8],
+    output_buffer: &mut [u8],
+    output_stride: u32,
+) -> Result<(), WebPSimpleError> {
+    assert!(output_stride as c_int >= 0);
+    assert_eq!(output_stride as c_int as u32, output_stride);
+    let result = unsafe {
+        sys::WebPDecodeRGBAInto(
+            data.as_ptr(),
+            data.len(),
+            output_buffer.as_mut_ptr(),
+            output_buffer.len(),
+            output_stride as c_int,
+        )
+    };
+    if !result.is_null() {
+        Ok(())
+    } else {
+        Err(WebPSimpleError)
+    }
+}
+
+#[allow(non_snake_case)]
+pub fn WebPDecodeARGBInto(
+    data: &[u8],
+    output_buffer: &mut [u8],
+    output_stride: u32,
+) -> Result<(), WebPSimpleError> {
+    assert!(output_stride as c_int >= 0);
+    assert_eq!(output_stride as c_int as u32, output_stride);
+    let result = unsafe {
+        sys::WebPDecodeARGBInto(
+            data.as_ptr(),
+            data.len(),
+            output_buffer.as_mut_ptr(),
+            output_buffer.len(),
+            output_stride as c_int,
+        )
+    };
+    if !result.is_null() {
+        Ok(())
+    } else {
+        Err(WebPSimpleError)
+    }
+}
+
+#[allow(non_snake_case)]
+pub fn WebPDecodeBGRAInto(
+    data: &[u8],
+    output_buffer: &mut [u8],
+    output_stride: u32,
+) -> Result<(), WebPSimpleError> {
+    assert!(output_stride as c_int >= 0);
+    assert_eq!(output_stride as c_int as u32, output_stride);
+    let result = unsafe {
+        sys::WebPDecodeBGRAInto(
+            data.as_ptr(),
+            data.len(),
+            output_buffer.as_mut_ptr(),
+            output_buffer.len(),
+            output_stride as c_int,
+        )
+    };
+    if !result.is_null() {
+        Ok(())
+    } else {
+        Err(WebPSimpleError)
+    }
+}
+
+#[allow(non_snake_case)]
+pub fn WebPDecodeRGBInto(
+    data: &[u8],
+    output_buffer: &mut [u8],
+    output_stride: u32,
+) -> Result<(), WebPSimpleError> {
+    assert!(output_stride as c_int >= 0);
+    assert_eq!(output_stride as c_int as u32, output_stride);
+    let result = unsafe {
+        sys::WebPDecodeRGBInto(
+            data.as_ptr(),
+            data.len(),
+            output_buffer.as_mut_ptr(),
+            output_buffer.len(),
+            output_stride as c_int,
+        )
+    };
+    if !result.is_null() {
+        Ok(())
+    } else {
+        Err(WebPSimpleError)
+    }
+}
+
+#[allow(non_snake_case)]
+pub fn WebPDecodeBGRInto(
+    data: &[u8],
+    output_buffer: &mut [u8],
+    output_stride: u32,
+) -> Result<(), WebPSimpleError> {
+    assert!(output_stride as c_int >= 0);
+    assert_eq!(output_stride as c_int as u32, output_stride);
+    let result = unsafe {
+        sys::WebPDecodeBGRInto(
+            data.as_ptr(),
+            data.len(),
+            output_buffer.as_mut_ptr(),
+            output_buffer.len(),
+            output_stride as c_int,
+        )
+    };
+    if !result.is_null() {
+        Ok(())
+    } else {
+        Err(WebPSimpleError)
+    }
+}
+
+#[allow(non_snake_case)]
+pub fn WebPDecodeYUVInto(
+    data: &[u8],
+    luma: &mut [u8],
+    luma_stride: u32,
+    u: &mut [u8],
+    u_stride: u32,
+    v: &mut [u8],
+    v_stride: u32,
+) -> Result<(), WebPSimpleError> {
+    assert!(luma_stride as c_int >= 0);
+    assert_eq!(luma_stride as c_int as u32, luma_stride);
+    assert!(u_stride as c_int >= 0);
+    assert_eq!(u_stride as c_int as u32, u_stride);
+    assert!(v_stride as c_int >= 0);
+    assert_eq!(v_stride as c_int as u32, v_stride);
+    let result = unsafe {
+        sys::WebPDecodeYUVInto(
+            data.as_ptr(),
+            data.len(),
+            luma.as_mut_ptr(),
+            luma.len(),
+            luma_stride as c_int,
+            u.as_mut_ptr(),
+            u.len(),
+            u_stride as c_int,
+            v.as_mut_ptr(),
+            v.len(),
+            v_stride as c_int,
+        )
+    };
+    if !result.is_null() {
+        Ok(())
+    } else {
+        Err(WebPSimpleError)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -229,5 +387,87 @@ mod tests {
         assert_eq!(&buf.y()[..6], &[165, 165, 165, 165, 162, 162]);
         assert_eq!(&buf.u()[..6], &[98, 98, 98, 98, 98, 98]);
         assert_eq!(&buf.v()[..6], &[161, 161, 161, 161, 161, 161]);
+    }
+
+    #[test]
+    #[allow(non_snake_case)]
+    fn test_WebPDecodeRGBAInto() {
+        let mut buf = vec![0; 128 * 128 * 4];
+        WebPDecodeRGBAInto(&lena(), &mut buf, 128 * 4).unwrap();
+        assert_eq!(
+            &buf[..24],
+            &[
+                226, 158, 113, 255, 226, 158, 113, 255, 226, 158, 113, 255, 226, 158, 113, 255,
+                223, 155, 109, 255, 223, 155, 109, 255,
+            ]
+        );
+    }
+
+    #[test]
+    #[allow(non_snake_case)]
+    fn test_WebPDecodeARGBInto() {
+        let mut buf = vec![0; 128 * 128 * 4];
+        WebPDecodeARGBInto(&lena(), &mut buf, 128 * 4).unwrap();
+        assert_eq!(
+            &buf[..24],
+            &[
+                255, 226, 158, 113, 255, 226, 158, 113, 255, 226, 158, 113, 255, 226, 158, 113,
+                255, 223, 155, 109, 255, 223, 155, 109,
+            ]
+        );
+    }
+
+    #[test]
+    #[allow(non_snake_case)]
+    fn test_WebPDecodeBGRAInto() {
+        let mut buf = vec![0; 128 * 128 * 4];
+        WebPDecodeBGRAInto(&lena(), &mut buf, 128 * 4).unwrap();
+        assert_eq!(
+            &buf[..24],
+            &[
+                113, 158, 226, 255, 113, 158, 226, 255, 113, 158, 226, 255, 113, 158, 226, 255,
+                109, 155, 223, 255, 109, 155, 223, 255,
+            ]
+        );
+    }
+
+    #[test]
+    #[allow(non_snake_case)]
+    fn test_WebPDecodeRGBInto() {
+        let mut buf = vec![0; 128 * 128 * 3];
+        WebPDecodeRGBInto(&lena(), &mut buf, 128 * 3).unwrap();
+        assert_eq!(
+            &buf[..24],
+            &[
+                226, 158, 113, 226, 158, 113, 226, 158, 113, 226, 158, 113, 223, 155, 109, 223,
+                155, 109, 223, 155, 109, 223, 155, 109,
+            ]
+        );
+    }
+
+    #[test]
+    #[allow(non_snake_case)]
+    fn test_WebPDecodeBGRInto() {
+        let mut buf = vec![0; 128 * 128 * 3];
+        WebPDecodeBGRInto(&lena(), &mut buf, 128 * 3).unwrap();
+        assert_eq!(
+            &buf[..24],
+            &[
+                113, 158, 226, 113, 158, 226, 113, 158, 226, 113, 158, 226, 109, 155, 223, 109,
+                155, 223, 109, 155, 223, 109, 155, 223
+            ],
+        );
+    }
+
+    #[test]
+    #[allow(non_snake_case)]
+    fn test_WebPDecodeYUVInto() {
+        let mut luma = vec![0; 128 * 128];
+        let mut u = vec![0; 64 * 64];
+        let mut v = vec![0; 64 * 64];
+        WebPDecodeYUVInto(&lena(), &mut luma, 128, &mut u, 64, &mut v, 64).unwrap();
+        assert_eq!(&luma[..6], &[165, 165, 165, 165, 162, 162]);
+        assert_eq!(&u[..6], &[98, 98, 98, 98, 98, 98]);
+        assert_eq!(&v[..6], &[161, 161, 161, 161, 161, 161]);
     }
 }
